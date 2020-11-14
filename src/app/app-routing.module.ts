fe7efd4928c0fb9bdd7cc4dbafe910441ebfded6
Myroslav Helesh <myroslavhelesh@gmail.com>
2020-11-15 01:54:34 +0200

    Angular: added side menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: '',
@@ -13,7 +13,7 @@ const routes: Routes = [
   },
   {
     path: 'journal',
-    loadChildren: () => import('./journal/journal.module').then( m => m.JournalPageModule)
+    loadChildren: () => import('./journal/journal.module').then(m => m.JournalPageModule)
   },
 ];
 
