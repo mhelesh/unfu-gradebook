21436657d957537bbe735f50d10e649e4efe7ee7
Myroslav Helesh <myroslavhelesh@gmail.com>
2020-11-15 00:29:57 +0200

    Firebase: setuped

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAicTZhaiT8L0G-nEbAIbQz23G7XaYmhfc',
+    authDomain: 'unfu-gradebook.firebaseapp.com',
+    databaseURL: 'https://unfu-gradebook.firebaseio.com',
+    projectId: 'unfu-gradebook',
+    storageBucket: 'unfu-gradebook.appspot.com',
+    messagingSenderId: '510547925560',
+    appId: '1:510547925560:web:bbafa2b763f384699da4cd',
+    measurementId: 'G-L1XWWX1BZT'
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAicTZhaiT8L0G-nEbAIbQz23G7XaYmhfc',
+    authDomain: 'unfu-gradebook.firebaseapp.com',
+    databaseURL: 'https://unfu-gradebook.firebaseio.com',
+    projectId: 'unfu-gradebook',
+    storageBucket: 'unfu-gradebook.appspot.com',
+    messagingSenderId: '510547925560',
+    appId: '1:510547925560:web:bbafa2b763f384699da4cd',
+    measurementId: 'G-L1XWWX1BZT'
+  },
 };
 
 /*
