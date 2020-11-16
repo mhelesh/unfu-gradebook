93e0aca2d01c46085576b38956e6038ec527858c
Myroslav Helesh <myroslavhelesh@gmail.com>
2020-11-17 00:50:33 +0200

    Angular: pills added

--- src/app/journal/journal.page.ts
+++ src/app/journal/journal.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Event } from '@angular/router';
 import { Observable } from 'rxjs';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 import { shareReplay, switchMap } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
 import { DbService } from '../services/db.service';
@@ -11,6 +13,9 @@ import { DbService } from '../services/db.service';
 })
 export class JournalPage implements OnInit {
   journals$: Observable<any>;
+  institutes$: Observable<any>;
+
+  chosenInstitute = new BehaviorSubject(null);
 
   constructor(public db: DbService, public auth: AuthService) { }
 
@@ -18,8 +23,20 @@ export class JournalPage implements OnInit {
     this.journals$ = this.auth.user$.pipe(
       switchMap(user => this.db.collection$('journals', ref =>
         ref
-          // .where('uid', '==', user.uid)
+          //.where('uid', '==', user.uid)
           // .orderBy('createAt', 'desc')
+          .limit(100)
+      )
+      ),
+      shareReplay(1)
+    );
+
+    this.institutes$ = this.db.doc$('enums/university');
+
+    this.journals$ = this.chosenInstitute.pipe(
+      switchMap(filter => this.db.collection$('journals', ref =>
+        ref
+          .where('inst', '==', filter)
           .limit(100)
       )
       ),
@@ -29,6 +46,15 @@ export class JournalPage implements OnInit {
 
   trackById(idx: string, journal: any): string {
     return journal.id;
+  }
+
+  updateChosenInstitute(val) {
+    this.chosenInstitute.next(val);
+  }
+
+  changeInstitute(event) {
+    console.log(event.detail.value);
+    this.updateChosenInstitute(event.detail.value);
   }
 
 }
