24f7383966d52e01466ee12bda60b256d3692203
Myroslav Helesh <myroslavhelesh@gmail.com>
2020-11-28 00:13:32 +0200

    Firebase: function added

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,36 +8,32 @@ import * as functions from 'firebase-functions';
 //   response.send("Hello from Firebase!");
 // });
 
-
 // Firebase
 import * as admin from 'firebase-admin';
-admin.initializeApp(functions.config().firebase);
-
+admin.initializeApp();
 
 // Cloud Vision
 import * as vision from '@google-cloud/vision';
 const visionClient = new vision.ImageAnnotatorClient();
 
-// Dedicated bucket for cloud function invocation
 const bucketName = 'unfu-gradebook-cloud-vision';
 
 export const imageTagger = functions.storage
     .bucket(bucketName)
     .object()
-    .onChange(async (event: { data: any; }) => {
-        const object = event.data;
-        const filePath = object.name;
+    .onFinalize(async (object, context) => {
+        const filePath = object.name || '';
 
-        const imageUri = `gs://${bucketName}/${filePath}`;
+        const imageUri = `gs://${bucketName}/${filePath}` || '';
 
-        const docId = filePath.split('.jpg')[0];
+        const docId = filePath?.split('.jpg')[0];
 
-        const docRef = admin.firestore().collection('photos').doc(docId);
+        const docRef = admin.firestore().collection('cloud-vision-photos').doc(docId);
 
         const results = await visionClient.labelDetection(imageUri);
 
-        const labels = results[0].labelAnnotations.map(obj => obj.description);
-        const hotdog = labels.includes('document');
+        const labels = results[0].labelAnnotations?.map(obj => obj.description) || [];
+        const document = labels.includes('Document');
 
-        return docRef.set({ hotdog, labels })
+        return docRef.set({ document, labels });
     });
